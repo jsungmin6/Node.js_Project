c6a552668e435cc08ac3285ddbbc6a468213dd2a
jsungmin6 <jsungmin506@gmail.com>
2020-10-31 20:18:55 +0900

    Revert "multer-s3 모듈 사용해 aws S3 스토리지에 이미지 업로드"
    This reverts commit 94500b0c919b5fdc5a8fd66bcf8e94015c4a815a.

--- NameScore_Project/config/multerMiddleware.js
+++ NameScore_Project/config/multerMiddleware.js
@@ -1,44 +1,25 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const multerS3 = require('multer-s3');
-const AWS = require("aws-sdk");
-const secret = require('./secret');
-AWS.config.loadFromPath(path.join(__dirname + "/awsconfig.json"));
 
-let s3 = new AWS.S3();
+try {
+    fs.readdirSync('uploads');
+} catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
 
-let upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: "manduawsbucket",
-        key: function (req, file, cb) {
-            let extension = path.extname(file.originalname);
-            cb(null, Date.now().toString() + extension)
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
         },
-        acl: 'public-read-write',
-    })
-})
-
-
-// try {
-//     fs.readdirSync('uploads');
-// } catch (error) {
-//     console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-//     fs.mkdirSync('uploads');
-// }
-
-// const upload = multer({
-//     storage: multer.diskStorage({
-//         destination(req, file, cb) {
-//             cb(null, 'uploads/');
-//         },
-//         filename(req, file, cb) {
-//             const ext = path.extname(file.originalname);
-//             cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-//         },
-//     }),
-//     limits: { fileSize: 5 * 1024 * 1024 },
-// });
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
 
 module.exports = upload;
--- NameScore_Project/src/web/routes/homeRoute.js
+++ NameScore_Project/src/web/routes/homeRoute.js
@@ -1,8 +1,6 @@
 module.exports = function (app) {
     const home = require('../controllers/homeController');
     const upload = require('../../../config/multerMiddleware');
-
-
     app.get('/names/:name', home.names);
     app.get('/uploads', home.uploads);
     app.route("/letters").post(home.letters);
