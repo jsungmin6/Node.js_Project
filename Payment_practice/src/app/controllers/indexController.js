ed54a0276cc96ed387595f4c929404b69c2966e4
jsungmin6 <jsungmin506@gmail.com>
2020-11-26 23:57:36 +0900

    story 정리

--- Payment_practice/src/app/controllers/indexController.js
+++ Payment_practice/src/app/controllers/indexController.js
@@ -17,8 +17,15 @@ var BuyerEmail = 'happy@day.com';
 var BuyerTel = '00000000000';
 var enc_data = "";
 var sign_data = "";
-
-
+// 1. 카드등록
+// 배열에 MID,EdiDate,Moid,EncData,SignData,BuyerName,BuyerEmail,BuyerTel,CharSet,EdiType 을 넣어야 함.
+// EncData 에는 CardNo, ExpYear, ExpMonth, IDNo, CardPw 가 들어가야 함
+// SignData 에는 MID, EdiDate, Moid, 상점키 들어가야 함
+// 암호화 hex, SHA256, aes-128-ecb 통해서 EncData 와 SignData 만들어야 함.
+// POST 메서드 사용
+// 헤더에 Content-Type:application/x-www-form-urlencoded 사용
+// Encoding : EUC-KR 
+// https://webapi.nicepay.co.kr/webapi/billing/billing_regist.jsp 로 보내야 함.
 
 exports.test = async function (req, res) {
     try {
